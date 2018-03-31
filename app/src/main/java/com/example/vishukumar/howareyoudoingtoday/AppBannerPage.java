37279321e2a93e7360463907ed0e20313cc15f6d
Vishukumar M <beingvishu@gmail.com>
2018-03-31 16:23:20 +0530

    Added activity to display chart.

--- app/src/main/java/com/example/vishukumar/howareyoudoingtoday/AppBannerPage.java
+++ app/src/main/java/com/example/vishukumar/howareyoudoingtoday/AppBannerPage.java
@@ -1,12 +1,21 @@
 package com.example.vishukumar.howareyoudoingtoday;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class AppBannerPage extends AppCompatActivity {
+
+    TextView appName, appSubtitle;
+    Typeface groBold;
+    Typeface helvatica;
+    Animation subtitleAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +23,16 @@ public class AppBannerPage extends AppCompatActivity {
         Log.d("tag", "onCreate");
         setContentView(R.layout.activity_app_banner_page);
 
+        appName = (TextView) findViewById(R.id.appNameTextViewId);
+        appSubtitle = (TextView) findViewById(R.id.appSubtitleTextViewId);
+        groBold = Typeface.createFromAsset(getAssets(), "GROBOLD.ttf");
+        helvatica = Typeface.createFromAsset(getAssets(), "HELR45W.ttf");
+        appName.setTypeface(groBold);
+        appSubtitle.setTypeface(helvatica);
+
+        subtitleAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.subtitle);
+        appSubtitle.setAnimation(subtitleAnimation);
+        /*
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -23,6 +42,7 @@ public class AppBannerPage extends AppCompatActivity {
                 finish();
             }
         }, 5000);
+        */
     }
 
 
